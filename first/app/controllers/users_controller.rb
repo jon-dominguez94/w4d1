ad5351387d4c271787685418b95fae6dee804a75
jon-dominguez94 <jon.dominguez94@yahoo.com>
2018-11-12 15:42:55 -0800

    Finish exercise

--- first/app/controllers/users_controller.rb
+++ first/app/controllers/users_controller.rb
@@ -5,10 +5,31 @@ class UsersController < ApplicationController
   end
   
   def create
-   render json: params
+    user = User.new(user_params)
+    if user.save
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
   end
   
   def show
-    render json: params
+    user = User.find(params[:id])
+    render json: user
+  end
+  
+  def update
+    user = User.find(params[:id])
+    user.update_attributes(user_params)
+  end
+  
+  def destroy
+    user = User.find(params[:id])
+    user.delete
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
