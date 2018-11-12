22b84817c82f6b0a46db9f3b02bad97fbf75e2aa
jon-dominguez94 <jon.dominguez94@yahoo.com>
2018-11-12 14:01:54 -0800

    Create routes

--- first/config/routes.rb
+++ first/config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
+  # resources :users
+  
+  get '/users', to: 'users#index'
+  get '/users/:id', to: 'users#show'
+  post '/users', to: 'users#create'
+  get '/users/new', to: 'users#new'
+  patch '/users/:id', to: 'users#update'
+  delete '/users/:id', to: 'users#destroy'
+  get '/users/:id/edit', to: 'users#edit'
+  put '/users/:id', to: 'users#update'
+  
 end
