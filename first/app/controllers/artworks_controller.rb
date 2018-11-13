fce21d0ba8ef7468c29f05ce4232176937d7acf1
jon-dominguez94 <jon.dominguez94@yahoo.com>
2018-11-12 17:26:00 -0800

    Create controller for artworks

--- first/app/controllers/artworks_controller.rb
+++ first/app/controllers/artworks_controller.rb
@@ -1,2 +1,40 @@
 class ArtworksController < ApplicationController
+  def index
+    # render plain: "I'm in the index action!"
+    render json: Artwork.all
+  end
+  
+  def create
+    artwork = Artwork.new(artwork_params)
+    if artwork.save
+      render json: artwork
+    else
+      render json: artwork.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+  
+  def show
+    artwork = Artwork.find(params[:id])
+    render json: artwork
+  end
+  
+  def update
+    artwork = Artwork.find(params[:id])
+    if artwork.update_attributes(artwork_params)
+      render json: artwork
+    else
+      render json: artwork.errors, status: :unprocessable_entity
+    end
+  end
+  
+  def destroy
+    artwork = Artwork.find(params[:id])
+    artwork.destroy
+    render json: artwork
+  end
+  
+  private
+  def artwork_params
+    params.require(:artwork).permit()
+  end
 end
