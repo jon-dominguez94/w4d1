5149688534b02e4192e563442690fe121cce183c
jon-dominguez94 <jon.dominguez94@yahoo.com>
2018-11-12 17:34:34 -0800

    FInish phase 3

--- first/config/routes.rb
+++ first/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only:[:create, :destroy, :index, :show, :update]
+  resources :artworks, only:[:create, :destroy, :index, :show, :update]
 # 
 #   get '/users', to: 'users#index'
 #   get '/users/:id', to: 'users#show', as: 'user'
