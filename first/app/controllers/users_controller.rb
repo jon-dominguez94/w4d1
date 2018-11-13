7fdeb6ed472bce027752fbe5e06115cd8d753022
jon-dominguez94 <jon.dominguez94@yahoo.com>
2018-11-12 17:22:25 -0800

    Create artworks controller

--- first/app/controllers/users_controller.rb
+++ first/app/controllers/users_controller.rb
@@ -29,12 +29,12 @@ class UsersController < ApplicationController
   
   def destroy
     user = User.find(params[:id])
-    user.delete
+    user.destroy
     render json: user
   end
   
   private
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:username)
   end
 end
